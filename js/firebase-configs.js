f6223ecdafeef5601b1d325a08f0d73c7da182a2
ForceNet-Corporation <forcenetcorporation@gmail.com>
2026-08-16 21:38:51 +0300

    Update firebase-configs.js

--- js/firebase-configs.js
+++ js/firebase-configs.js
@@ -1,13 +1,13 @@
 const firebaseConfig = {
     apiKey: "AIzaSyCm9GeIcOtrR3cbWv0fSash5sT7sL9S1rk",
-    aythDomain: "testwebapp-4db03.firebaseapp.com",
+    authDomain: "testwebapp-4db03.firebaseapp.com",
     projectId: "testwebapp-4db03",
     storageBucket: "testwebapp-4db03.firebasestorage.app",
     messagingSenderId: "838164449927",
     appId: "1:838164449927:web:279f3b6e72fdffabde0622"
 };
 
-firebaseConfig.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
 
